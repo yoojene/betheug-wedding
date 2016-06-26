280d57e43426f67776f519bc63f2ebfa1beff6f0
Eugene <eugene.cross@eams-group.com>
2016-06-26 23:23:27 +0100

    Accommodation and info updates.  email form validation updated

--- www/js/formsubmit.js
+++ www/js/formsubmit.js
@@ -2,16 +2,12 @@ $(function() {
 
 	$('#submitForm').attr('disabled', 'disabled');
 
-
-	//$( "#message" ).blur(function() { // Tab out of message field
-	$( "#message" ).change(function() {
-		console.log('message has chaanged');
-	if ($("#name").val().length > 0 && $("#email").val().length > 0 && $("#message").val().length > 0 && $("#formResp").text().length > 0){
+	$( "#yesBtn, #noBtn" ).click(function() {
+		console.log('yesBtn or noBtn has chaanged');
+	if ($("#name").val().length > 0 && $("#email").val().length > 0 && $("#formResp").text().length > 0){
 		$('#submitForm').removeAttr('disabled');
 	}
 });
-
-
 
 
     $('#submitForm').click(function() {
